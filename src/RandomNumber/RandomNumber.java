b2637c3421a71860925c1b2b903da8a6ce09300d
Aakanshad28 <aakanshadahekar2001@gmail.com>
2023-12-27 20:39:30 +0530

    Initial commit

---
+++ src/RandomNumber/RandomNumber.java
@@ -0,0 +1,29 @@
+package RandomNumber;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumber {
+	public static void main(String[] args) {
+		Random ran = new Random();
+		int randomNumber = ran.nextInt(100) + 1;
+		//System.out.println("The Random Number is:" + randomNumber);
+		while (true) {
+
+			System.out.println("Enter your guessing number between(1-100):");
+			Scanner scanner = new Scanner(System.in);
+			
+			int guess = scanner.nextInt();
+			if (guess == randomNumber) {
+				System.out.println("You Win!Your Guessing is correct.");
+				break;
+			} else if (randomNumber > guess) {
+				System.out.println("Please try again!The number is greater than the guess number");
+			} else {
+				System.out.println("Please try again!The number is lesser than the guess number");
+			}
+		}
+
+	}
+
+}
